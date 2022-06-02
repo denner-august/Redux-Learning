c02ebec8611ce920fc90bdbf77aadf2d6c1d069e
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-02 11:12:02 -0300

    ajustes para deploy

--- redux-learning/src/App.tsx
+++ redux-learning/src/App.tsx
@@ -1,19 +1,11 @@
 import Header from "./components/header/index";
-import BackdropExample from "./components/modal";
 import ListToDo from "./components/to-do";
 
-import { useSelector } from "react-redux";
-
 export default function App() {
-  const count = useSelector(
-    (state: { configModal: { value: boolean } }) => state.configModal
-  );
-
   return (
     <div>
       <Header />
       <ListToDo />
-      <BackdropExample openModal={count.value} />
     </div>
   );
 }
--- redux-learning/src/redux/store.tsx
+++ redux-learning/src/redux/store.tsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import configModal from "./reducer";
 import ListConfig from "./reducerList";
 
 export default configureStore({
   reducer: {
-    configModal: configModal,
     ListConfig: ListConfig,
   },
 });
